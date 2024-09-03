d4defb0b327ff9d1c7bbea8b12b1fa7a8884c753
Lochlan Dunne <ignobliss42@gmail.com>
2024-09-03 13:35:41 +1000

    Update to AngleControl
    Update to AngleControl with code that causes the calculation for the angle and the publishing of that data to occur when the player collides with something and the player remains in contact with the terrain, so long as the player has a non-kinematic rigidbody. Should fix the issues Ethan is having with testing though I don't know why lack of collision triggers would affect the code since it no longer uses collisions in the calculations.

--- Assets/Scripts/MQTT/AngleControl.cs
+++ Assets/Scripts/MQTT/AngleControl.cs
@@ -54,6 +54,48 @@ public class AngleControl : MonoBehaviour
         }
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+       Vector3 playerVector = Player.transform.forward;
+
+       InGameAngle = Player.transform.rotation.eulerAngles.x;
+       
+       BikeAngle = RefineAngle(InGameAngle);
+
+       timeInterval += Time.deltaTime;
+        if (timeInterval >= publishPeriod)
+        {
+            var ts = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+            float incline = Mathf.Clamp(BikeAngle, minimumAngle, maximumAngle);
+            string payload = "{\"ts\": " + ts + ", \"incline\": " + incline + "}";
+            mqtt.Publish(mqtt.inclineTopic, payload);
+
+            BikeAngle = incline;
+            timeInterval = 0.0f;
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+       Vector3 playerVector = Player.transform.forward;
+
+       InGameAngle = Player.transform.rotation.eulerAngles.x;
+       
+       BikeAngle = RefineAngle(InGameAngle);
+
+       timeInterval += Time.deltaTime;
+        if (timeInterval >= publishPeriod)
+        {
+            var ts = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+            float incline = Mathf.Clamp(BikeAngle, minimumAngle, maximumAngle);
+            string payload = "{\"ts\": " + ts + ", \"incline\": " + incline + "}";
+            mqtt.Publish(mqtt.inclineTopic, payload);
+
+            BikeAngle = incline;
+            timeInterval = 0.0f;
+        }
+    }
+
     private float RefineAngle(float RawAngle)
     {
         float Refined;
