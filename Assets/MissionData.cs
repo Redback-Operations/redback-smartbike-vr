386f05002877dc7a3ea03e69198eaf50e9e883b8
djasmit <djsmit@deakin.edu.au>
2025-03-24 17:08:25 +1100

    UI Update
    UI Update

---
+++ Assets/MissionData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class MissionData : MonoBehaviour
+{
+    public String DescriptionText;
+    public string TargetScene;
+    public int MissionID;
+}
