a37b2f7aa5c061a8e1ae03e841a1e1f6b5caa92c
djasmit <djsmit@deakin.edu.au>
2025-09-21 22:26:20 +1000

    Optimize Boost Pads
    Changes:
    
    Scripts:
    Assets/Scripts/Gameplay/Missions/Mission7/RoadTile.cs:
    - added new SpawnBoost() function that raycasts from a random point on planes above a tile to find a point on the tile. Then does corner raycasts to ensure that the boost pad won't spawn over an edge.
    - SpawnBoostRamp function now uses the SpawnBoost() function to determine if a boost pad can be spawned, and if so what the position and rotation will be
    - SpawnBoostRamp() function no longer hardcodes 2 as the boost pad number, but instead uses a new maxBoostPads variable that can be set per tile (set to 2 for most, but larger tiles have more)
    - replaced references to Random with UnityEngine.Random to avoid ambiguity
    
    Assets/Prefabs/Roads/[all road tiles]:
    - added new raycast spawning planes to each tile
    - added values to the new maxBoostPads variable

--- Assets/Scripts/Gameplay/Missions/Mission7/RoadTile.cs
+++ Assets/Scripts/Gameplay/Missions/Mission7/RoadTile.cs
@@ -1,9 +1,12 @@
 using ExitGames.Client.Photon.StructWrapping;
+using Nobi.UiRoundedCorners;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Timeline;
 
 [System.Serializable]
 
@@ -15,6 +18,8 @@ public class RoadTile : MonoBehaviour
     public bool bHasBoosts;
     public bool spawnsObstacles;
     public GameObject TileSurface;
+    public BoxCollider RaycastSpawner;
+    public int maxBoostPads;
 
     // Start is called before the first frame update
     private void Start()
@@ -45,8 +50,8 @@ public class RoadTile : MonoBehaviour
         if (obstacleSpawnPoints.Count == 0 || obstacleSpawnPoints == null) return;
 
         //updated to select obstacle spawn point from list of spawns
-        int itemIndex = Random.Range(0, obstaclePrefabs.Count);
-        int itemSpawnIndex = Random.Range(0, obstacleSpawnPoints.Count);
+        int itemIndex = UnityEngine.Random.Range(0, obstaclePrefabs.Count);
+        int itemSpawnIndex = UnityEngine.Random.Range(0, obstacleSpawnPoints.Count);
         Transform spawnPoint = obstacleSpawnPoints[itemSpawnIndex].transform;
 
         Instantiate(obstaclePrefabs[itemIndex], spawnPoint.position, Quaternion.identity, transform);
@@ -61,8 +66,8 @@ public class RoadTile : MonoBehaviour
             return;
         }
 
-        int rampToSpawn = 2;
-        for (int i = 0; i < rampToSpawn; i++)
+        //removed hardcoded 2 for pad variable (default to 2)
+        for (int i = 0; i < maxBoostPads; i++)
         {
             GameObject temp = Instantiate(boostRampPrefab, transform);
             try
@@ -71,11 +76,18 @@ public class RoadTile : MonoBehaviour
                 if (collider = null) throw new System.Exception("Invalid collider!");
 
                 //find spawn point on the tile for the instantiated boostpad
-                temp.transform.position = GetRandomPointInCollider(TileSurface.transform.GetComponent<Collider>(), temp.transform.GetComponent<Collider>());
-                Debug.Log($"Height: {temp.transform.position}");
-                temp.transform.rotation = TileSurface.transform.rotation;
+                Vector3 spawnPos;
+                Quaternion spawnRot;
 
-                Debug.Log($"BoostRamp spawned at: {temp.transform.position}");
+                if (SpawnBoost(RaycastSpawner, TileSurface, temp.transform.GetComponent<BoxCollider>(), out spawnPos, out spawnRot)) {
+                    temp.transform.SetPositionAndRotation(spawnPos, spawnRot);
+                }
+                else
+                {
+                    Destroy(temp); // remove unnused boosts
+                }
+
+                    Debug.Log($"BoostRamp spawned at: {temp.transform.position}");
             }
             catch (System.Exception e)
             {
@@ -86,7 +98,7 @@ public class RoadTile : MonoBehaviour
         }
     }
 
-    //evil recursive function blows up when no valid collider
+    //evil recursive function blows up when no valid collider - DEPRECIATED
     Vector3 GetRandomPointInCollider(Collider tileCollider, Collider toSpawnCollider)
     {
         if (tileCollider == null) throw new System.Exception("Invalid tile collider!");
@@ -98,9 +110,9 @@ public class RoadTile : MonoBehaviour
 
         //chose random point between bounds of tile + half size of new object
         Vector3 point = new Vector3(
-            Random.Range(tileCollider.bounds.min.x + toSpawnSize.x, tileCollider.bounds.max.x - toSpawnSize.x),
+            UnityEngine.Random.Range(tileCollider.bounds.min.x + toSpawnSize.x, tileCollider.bounds.max.x - toSpawnSize.x),
             tileCollider.bounds.max.y,
-            Random.Range(tileCollider.bounds.min.z + toSpawnSize.z, tileCollider.bounds.max.z - toSpawnSize.z)
+            UnityEngine.Random.Range(tileCollider.bounds.min.z + toSpawnSize.z, tileCollider.bounds.max.z - toSpawnSize.z)
         );
 
         Debug.Log($"Point: {point}");
@@ -113,4 +125,92 @@ public class RoadTile : MonoBehaviour
         Debug.Log($"Generated point: {point}");
         return point;
     }
+
+
+    //POLISH THIS
+    public bool SpawnBoost(BoxCollider raycastPlane, GameObject tileCollider, BoxCollider toSpawnCollider, out Vector3 spawnPos, out Quaternion spawnRot)
+    {
+        if (raycastPlane == null || toSpawnCollider == null || tileCollider == null) throw new System.Exception("Missing collider!");
+        int maxAttempts = 10;
+
+        //make several attempts to find a valid spawn location
+        for (int i = 0; i < maxAttempts; i++)
+        {
+            //find a point on the plane of the raycast spawner
+            float localX = UnityEngine.Random.Range(-0.5f, 0.5f) * raycastPlane.size.x;
+            float localZ = UnityEngine.Random.Range(-0.5f, 0.5f) * raycastPlane.size.z;
+            float bufferDistance = 0.05f;
+
+            Vector3 localPoint = new Vector3(localX, 0f, localZ) + raycastPlane.center;
+            Vector3 worldPoint = raycastPlane.transform.TransformPoint(localPoint);
+            Debug.Log($"Local Point: {localPoint}, World Point: {worldPoint}");
+
+            //make the scan and try to find a valid point
+            RaycastHit centreHit;
+            if (Physics.Raycast(worldPoint, Vector3.down, out centreHit, 10) && centreHit.transform.IsChildOf(tileCollider.gameObject.transform))
+            {
+                var marker = new GameObject("CentreMarker");
+                marker.transform.SetPositionAndRotation(centreHit.point, Quaternion.FromToRotation(Vector3.up, centreHit.normal));
+                marker.transform.SetParent(transform, true);
+
+                float toSpawnWidth = toSpawnCollider.size.x / 2 + bufferDistance;
+                float toSpawnLength = toSpawnCollider.size.z / 2 + bufferDistance;
+
+                //finding corners along axis of the potential spawn
+                var normalRot = Quaternion.FromToRotation(Vector3.up, centreHit.normal);
+                var normalRight = normalRot * Vector3.right;
+                var normalFwd = normalRot * Vector3.forward;
+                var temp = centreHit.point; temp.y += toSpawnCollider.size.y + bufferDistance;
+
+                Vector3[] corners = new Vector3[4];
+                corners[0] = temp; corners[0] = temp + (-normalRight * toSpawnWidth) + (-normalFwd * toSpawnLength);
+                corners[1] = temp; corners[1] = temp + (-normalRight * toSpawnWidth) + (normalFwd * toSpawnLength);
+                corners[2] = temp; corners[2] = temp + (normalRight * toSpawnWidth) + (-normalFwd * toSpawnLength);
+                corners[3] = temp; corners[3] = temp + (normalRight * toSpawnWidth) + (normalFwd * toSpawnLength);
+
+                //spawn a ray for each of the projected corners to see if we're in a valid spot
+                bool validCorner = false;
+                foreach (Vector3 corner in corners)
+                {
+                    bool impact = false;
+                    bool validImpact = false;
+                    RaycastHit cornerHit;
+
+                    Debug.Log($"Projected Point: {corner}");
+
+
+                    //if we go a little below the threshold and find a spot that is part of the road tile, then this is probably a valid point
+                    impact = Physics.Raycast(corner, -centreHit.normal, out cornerHit, toSpawnCollider.size.y + bufferDistance * 2);
+                    if (!impact) { validCorner = false; break; }
+
+                    Debug.Log($"Spawning GameObject for : {corner}");
+                    Quaternion cornerRot = Quaternion.FromToRotation(Vector3.up, cornerHit.normal);
+
+                    //debug purposes if needed
+                    marker = new GameObject("CornerMarker");
+                    marker.transform.SetPositionAndRotation(cornerHit.point, cornerRot);
+                    marker.transform.SetParent(transform, true);
+
+                    //make sure we haven't impact an obstacle or impacted outside our buffer range
+                    validImpact = (cornerHit.transform.IsChildOf(centreHit.transform));
+                    if (!validImpact) { validCorner = false; break; }
+
+                    validCorner = true;
+                }
+
+                if (!validCorner) break;
+
+                Quaternion rotation = Quaternion.FromToRotation(Vector3.up, centreHit.normal);
+                //Instantiate(boostRampPrefab, centreHit.point, rotation, transform);
+
+                Debug.Log($"BoostRamp spawned at: {centreHit.point}");
+                spawnPos = centreHit.point;
+                spawnRot = rotation;
+                return true;
+            }
+        }
+        spawnPos = Vector3.zero; spawnRot = Quaternion.identity;
+        return false;
+    }
+
 }
