441d345062f2d862be26ee251324d248a6f539b1
DanielM <s224477774@deakin.edu.au>
2026-07-15 18:07:07 +1000

    Fixing Demo File
    Fixing issues related to a demo.cs script causing the project to not compile properly.

--- Assets/UI/Timer UI/Countdown UI/Demo.cs
+++ Assets/UI/Timer UI/Countdown UI/Demo.cs
@@ -1,5 +1,24 @@
+
+
 using UnityEngine;
 
+public class Demo : MonoBehaviour
+{
+    [SerializeField] Timer2 timer1;
+    [SerializeField] int timerDuration = 15;
+    [SerializeField] float startDelay = 3f;
+
+    private void Start()
+    {
+        timer1 = FindObjectOfType<Timer2>();
+        timer1.SetDuration(timerDuration).BeginWithDelay(startDelay);
+    }
+}
+
+
+/* This script was in this state upon reception of the project in T2 2026. I can't find any uses of this script, but I've made a 'replacement' anyway. If anyone else finds themselves here the past script is here for your info if you're trying to fix something
+-DM
+using UnityEngine;
 public class Demo : MonoBehaviour
 {
     [SerializeField] CountdownTimerUI timer1;
@@ -12,6 +31,7 @@ public class Demo : MonoBehaviour
 
     [SerializeField] private int timerDuration = 15;
     [SerializeField] private float startDelay = 3f;
+    }
 
     private void Start()
     {
@@ -20,4 +40,4 @@ public class Demo : MonoBehaviour
         timer1.SetDuration(timerDuration)
               .BeginWithDelay(startDelay);
     }
-}
+} */
